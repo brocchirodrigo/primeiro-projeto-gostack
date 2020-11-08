bdb6bac061d169216809b4f7b58047c1241c3117
Rodrigo Brocchi <rodrigobrocchi@MacBook-Pro-de-Rodrigo-Brocchi.local>
2020-11-08 18:37:38 -0300

    Upload User Avatar

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '140f7188c30f1946382ac3ed351e453f',
+    expiresIn: '1d',
+  },
+};
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../service/CreateUserService';
+import UpdateUserAvatarService from '../service/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const UserRouter = Router();
+const upload = multer(uploadConfig);
 
 UserRouter.post('/', async (request, response) => {
   try {
@@ -23,5 +29,27 @@ UserRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+UserRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateAvatar = new UpdateUserAvatarService();
+
+      const user = await updateAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default UserRouter;
