41455de40af5c94a5287e7e0acb66c00cde232d1
Rodrigo Brocchi <rodrigobrocchi@MacBook-Pro-de-Rodrigo-Brocchi.local>
2020-11-08 19:15:51 -0300

    ErrorHandler Async Express

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,23 +11,19 @@ const UserRouter = Router();
 const upload = multer(uploadConfig);
 
 UserRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 UserRouter.patch(
@@ -35,20 +31,16 @@ UserRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateAvatar = new UpdateUserAvatarService();
+    const updateAvatar = new UpdateUserAvatarService();
 
-      const user = await updateAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const user = await updateAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(err.statusCode).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
