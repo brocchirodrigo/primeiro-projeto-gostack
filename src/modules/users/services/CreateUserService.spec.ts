993f271089b4b9f83c71e4e5daebad8ed84932dc
brocchirodrigo <rodrigo.brocchi@gmail.com>
2020-12-26 13:58:04 -0300

    Novo serviço: Forgot Password Email com teste

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,17 +1,17 @@
 import AppError from '@shared/errors/AppError';
 
-import FakeAppointmentsRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('Should be able to create a new user', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
 
     const createUser = new CreateUserService(
-      fakeAppointmentsRepository,
+      fakeUsersRepository,
       fakeHashProvider,
     );
 
@@ -25,11 +25,11 @@ describe('CreateUser', () => {
   });
 
   it('Should not be able to create a new user woth same email from another', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
 
     const createUser = new CreateUserService(
-      fakeAppointmentsRepository,
+      fakeUsersRepository,
       fakeHashProvider,
     );
 
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,30 @@
+import { injectable, inject } from 'tsyringe';
+// import User from '@modules/users/infra/typeorm/entities/User';
+
+// import AppError from '@shared/errors/AppError';
+import IUserRepository from '@modules/users/repositories/IUserRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UserRepository')
+    private userRepository: IUserRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
