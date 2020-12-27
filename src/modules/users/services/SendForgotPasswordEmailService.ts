54e1da376e47053f8ecaecf9fc7434abb172b854
brocchirodrigo <rodrigo.brocchi@gmail.com>
2020-12-27 17:30:16 -0300

    Recuperação para reset de senha DEV

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -30,11 +30,13 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists');
     }
 
-    await this.userTokenRepository.generate(checkUsersExists.id);
+    const { token } = await this.userTokenRepository.generate(
+      checkUsersExists.id,
+    );
 
-    this.mailProvider.sendMail(
+    await this.mailProvider.sendMail(
       email,
-      'Pedido de recuperação de senha recebido',
+      `Pedido de recuperação de senha recebido | Token: ${token}`,
     );
   }
 }
